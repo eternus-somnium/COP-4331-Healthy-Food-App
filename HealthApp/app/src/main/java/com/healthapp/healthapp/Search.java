f14669d5f86974990eb72e89f7f83b3d9bf80494
BryenBuie <blackgold222@gmail.com>
2015-10-27 10:27:52 -0400

    Changed search bar

--- HealthApp/app/src/main/java/com/healthapp/healthapp/Search.java
+++ HealthApp/app/src/main/java/com/healthapp/healthapp/Search.java
@@ -1,16 +1,44 @@
 package com.healthapp.healthapp;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class Search extends AppCompatActivity {
+
+    private EditText searchField;
+    private String sField;
+    ImageButton sButton;
+    ImageButton cButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        // Initializing text field
+        searchField = (EditText) findViewById(R.id.search_field);
+        sField = searchField.getText().toString();
+
+        // Initialize image button
+        sButton = (ImageButton) findViewById(R.id.imageButton);
+
+        cButton = (ImageButton) findViewById(R.id.clear_button);
+
+
+        // Set Text Watcher
+        searchField.addTextChangedListener(myWatcher);
+        // Set Click Listener
+        cButton.setOnClickListener(clearListener);
     }
 
     @Override
@@ -35,5 +63,51 @@ public class Search extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private View.OnClickListener clearListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            searchField = (EditText) findViewById(R.id.search_field);
+            sField = searchField.getText().toString();
+
+            searchField.setText("");
+        }
+    };
+    private final TextWatcher myWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            searchField = (EditText) findViewById(R.id.search_field);
+            sField = searchField.getText().toString();
+
+            // Initialize image button
+            sButton = (ImageButton) findViewById(R.id.imageButton);
+            if(sField.matches("")){
+                sButton.setImageResource(R.drawable.scanner);
+                cButton = (ImageButton) findViewById(R.id.clear_button);
+                cButton.setVisibility(View.INVISIBLE);
+            }
+            else {
+                sButton.setImageResource(R.drawable.search);
+                searchField = (EditText) findViewById(R.id.search_field);
+                sField = searchField.getText().toString();
+
+                // Initialize image button
+                sButton = (ImageButton) findViewById(R.id.imageButton);
+
+                cButton = (ImageButton) findViewById(R.id.clear_button);
+                cButton.setVisibility(View.VISIBLE);
+
+            }
+        }
+    };
 
 }
