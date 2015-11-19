be5d443aa9ce1a906026663985dab9c1af56c44b
BryenBuie <blackgold222@gmail.com>
2015-11-19 10:50:35 -0500

    results, signup, login

--- HealthApp/app/src/main/java/com/healthapp/healthapp/Login.java
+++ HealthApp/app/src/main/java/com/healthapp/healthapp/Login.java
@@ -1,18 +1,34 @@
 package com.healthapp.healthapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Login extends AppCompatActivity {
+
+    Connection connect = DatabaseAccess.Connect();
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // Initialize done button
+        loginButton = (Button) findViewById(R.id.login_button);
+
+        // Set Click Listener
+        loginButton.setOnClickListener(loginListener);
     }
 
     @Override
@@ -37,8 +53,58 @@ public class Login extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    private View.OnClickListener loginListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            //Do Login
+            EditText username = (EditText) findViewById(R.id.username);
+            EditText password = (EditText) findViewById(R.id.password);
+            int valid = 0;
+            try {
+                valid = DatabaseAccess.CheckUsernamePassword(username.toString(), password.toString(), connect);
+            } catch (SQLException e) {
+                AlertDialog alertDialog = new AlertDialog.Builder(Login.this).create();
+                alertDialog.setTitle("Alert");
+                alertDialog.setMessage("Alert message to be shown");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+
+            if(valid == 0)
+            {
+                gotoSearch(v);
+            }
+
+            else
+            {
+                AlertDialog alertDialog = new AlertDialog.Builder(Login.this).create();
+                alertDialog.setTitle("Alert");
+                alertDialog.setMessage("Alert message to be shown");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        }
+    };
+
     public void gotoSearch(View v) {
         Intent intent = new Intent(this,Search.class);
+        startActivity(intent);
+    }
+
+    public void gotoSignUp(View v) {
+        Intent intent = new Intent(this,SignUpActivity.class);
         startActivity(intent);
     }
 }
--- HealthApp/app/src/main/java/com/healthapp/healthapp/SignUpActivity.java
+++ HealthApp/app/src/main/java/com/healthapp/healthapp/SignUpActivity.java
@@ -1,16 +1,37 @@
 package com.healthapp.healthapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    Connection connect = DatabaseAccess.Connect();
+    Button doneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        // Initialize done button
+        doneButton = (Button) findViewById(R.id.done_button);
+
+        // Set Click Listener
+        doneButton.setOnClickListener(doneListener);
     }
 
     @Override
@@ -34,4 +55,74 @@ public class SignUpActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void SignUp()
+    {
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText password = (EditText) findViewById(R.id.password);
+        EditText foodkey = (EditText) findViewById(R.id.foodkey);
+        EditText barcodekey = (EditText) findViewById(R.id.barcodekey);
+
+        int valid = 0;
+
+        // Validate username
+        try {
+            valid = DatabaseAccess.ValidateUsername(username.toString(), connect);
+        } catch (SQLException e) {
+            AlertDialog alertDialog = new AlertDialog.Builder(SignUpActivity.this).create();
+            alertDialog.setTitle("Alert");
+            alertDialog.setMessage("Alert message to be shown");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+        }
+
+        // Insert user in database
+        if(valid == 0)
+        {
+            try {
+                DatabaseAccess.InsertUsername(username.toString(), password.toString(), connect);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Insert Food Key
+        if(valid == 0)
+        {
+            try {
+                DatabaseAccess.InsertFoodKey(username.toString(), password.toString(), foodkey.toString(), connect);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Insert Barcode Key
+        if(valid == 0)
+        {
+            try {
+                DatabaseAccess.InsertBarcodeKey(username.toString(), password.toString(), barcodekey.toString(), connect);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void gotoLogin(View v) {
+        Intent intent = new Intent(this,Login.class);
+        startActivity(intent);
+    }
+
+    private View.OnClickListener doneListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            SignUp();
+            gotoLogin(v);
+        }
+    };
 }
